732045c03fb6467bb9e076ccd4482b9e5a29dc33
Pierre Castets <pierre.castets@centrale-marseille.fr>
2021-09-22 09:15:09 +0200

    ajout exo4

---
+++ exo4.py
@@ -0,0 +1,24 @@
+'''
+*** Exo 4: somme de saisies ***
+Créer un programme demandant à l'utilisateur se saisir
+un chiffre. Tant que l'utilisateur ne saisit pas la valeur "0",
+on lui redemande la saisie d'un chiffre.
+En sortie de boucle, on affichera la somme des valeurs saisies ainsi qu'un
+récapitulatif des valeurs saisies
+Exemples de valeurs saisies par l'utilisateur:
+15
+2
+3
+0
+=> Somme des valeurs saisies: 20 (15,2,3)
+'''
+print("*** EXO 4: somme de saisies ***")
+
+sum = 0
+inputs = []
+number = None
+while (number != "0"):
+    number = input("Entrez un nombre positif ou nul: ")
+    sum += int(number)
+    inputs.append(number)
+print("Somme des valeurs saisies: ", sum, inputs)
