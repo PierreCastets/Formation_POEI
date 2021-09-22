1db7238cf778de8778996a1fff3c80709fda2ac1
Pierre Castets <pierre.castets@centrale-marseille.fr>
2021-09-22 17:57:14 +0200

    ajout exo6

--- exo6.py
+++ exo6.py
@@ -10,4 +10,36 @@ Le mot de passe devra contenir:
 La longeur sera donnée par une saisie utilisateur
 ex: longueur: 8 => Hn_y9l2%
 '''
-print("*** EXO 5: Générateur de mot de passe ***")
+import string, random
+
+print("*** EXO 6: Générateur de mot de passe ***")
+
+length = 0
+try:
+    while length < 4:
+        length = int(input("Entrez la taille du mot de passe (un nombre supérieur à 4): "))
+except:
+    print("L'entrée n'est pas valable")
+    exit()
+
+password = ""
+alphabet_lower = list(string.ascii_lowercase)
+alphabet_upper = list(string.ascii_uppercase)
+specials = list(string.punctuation)
+digits = list(string.digits)
+
+password += random.choice(alphabet_lower)
+password += random.choice(alphabet_upper)
+password += random.choice(specials)
+password += random.choice(digits)
+
+allCaracters = alphabet_lower + alphabet_upper + specials + digits
+
+for type in range(length-4):
+    password += random.choice(allCaracters)
+
+password_list = list(password)
+random.shuffle(password_list)
+password = ''.join(password_list)
+
+print("Proposition de mot de passe:",password)
